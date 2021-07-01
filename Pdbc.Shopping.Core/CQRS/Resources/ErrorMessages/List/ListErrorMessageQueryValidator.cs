be9a2840ea4d2ecfa49f35500e61866f6760764b
Patrick De Boeck <patrick.deboeck@sdworx.com>
2021-07-01 16:04:34 +0200

    Validator tests

---
+++ Pdbc.Shopping.Core/CQRS/Resources/ErrorMessages/List/ListErrorMessageQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Pdbc.Shopping.Core.Validation;
+using Pdbc.Shopping.I18N;
+
+namespace Pdbc.Shopping.Core.CQRS.Resources.ErrorMessages.List
+{
+    public class ListErrorMessageQueryValidator : FluentValidationValidator<ListErrorMessagesQuery>
+    {
+        public ListErrorMessageQueryValidator()
+        {
+            RuleFor(i => i.Language)
+                .NotEmpty()
+                .WithErrorCode(nameof(ErrorResources.LanguageIsEmpty));
+        }
+    }
+}
---
+++ Tests/Pdbc.Shopping.UnitTests/Core/CQRS/Resources/ErrorMessages/List/ListErrorMessagesQueryValidatorTestFixture.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using Pdbc.Shopping.Common.Validation;
+using Pdbc.Shopping.Core.CQRS.Resources.ErrorMessages.Get;
+using Pdbc.Shopping.Core.CQRS.Resources.ErrorMessages.List;
+using Pdbc.Shopping.I18N;
+using Pdbc.Shopping.Tests.Helpers;
+using Pdbc.Shopping.Tests.Helpers.Core.CQRS.Resources.ErrorMessages;
+using Pdbc.Shopping.Tests.Helpers.Core.Validation;
+
+namespace Pdbc.Shopping.UnitTests.Core.CQRS.Resources.ErrorMessages.List
+{
+    public class ListErrorMessagesQueryValidatorTestFixture : ContextSpecification<ListErrorMessageQueryValidator>
+    {
+        private ValidationBag _validationBag;
+
+        protected override void Establish_context()
+        {
+            base.Establish_context();
+
+           _validationBag = new ValidationBag();
+        }
+
+        [Test]
+        public void Verify_error_when_language_is_null()
+        {
+            var query = new ListErrorMessagesQueryTestDataBuilder().WithLanguage(null);
+            SUT.Validate(query, _validationBag).GetAwaiter().GetResult();
+            _validationBag.ExpectErrorWithCode(nameof(ErrorResources.LanguageIsEmpty));
+        }
+
+        [Test]
+        public void Verify_error_when_language_is_empty_string()
+        {
+            var query = new ListErrorMessagesQueryTestDataBuilder().WithLanguage("");
+            SUT.Validate(query, _validationBag).GetAwaiter().GetResult();
+            _validationBag.ExpectErrorWithCode(nameof(ErrorResources.LanguageIsEmpty));
+        }
+
+        [Test]
+        public void Verify_error_when_language_is_spaces_string()
+        {
+            var query = new ListErrorMessagesQueryTestDataBuilder().WithLanguage("   ");
+            SUT.Validate(query, _validationBag).GetAwaiter().GetResult();
+            _validationBag.ExpectErrorWithCode(nameof(ErrorResources.LanguageIsEmpty));
+        }
+
+    }
+}
